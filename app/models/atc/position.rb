2ea9ac18a2b700e4bca776abe7be6decd8760faf
Matthew J. Kosmoski <mkosmo@gmail.com>
2018-02-18 22:18:19 -0600

    clean up model

--- app/models/atc/position.rb
+++ app/models/atc/position.rb
@@ -51,15 +51,4 @@ class Atc::Position < ApplicationRecord
   validates :visible,
             inclusion: { in: [true, false] }
 
-
-  # t.string :name, null: false
-  # t.string :callsign, null: false
-  # t.string :callsign_prefix, null: false, limit: 3
-  # t.string :sector_id, limit: 3
-  # t.string :callsign_suffix, null: false, limit: 3
-  # t.decimal :frequency, null: false, default: 199.998
-  # t.boolean :designated, null: false, default: false
-  # t.boolean :primary, null: false, default: false
-  # t.integer :sortorder
-  # t.boolean :visible, null: false, default: false
 end
