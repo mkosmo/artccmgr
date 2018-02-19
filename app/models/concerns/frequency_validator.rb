fd808498b94b736a57446ee7e09891c987919507
Matthew J. Kosmoski <mkosmo@gmail.com>
2018-02-19 09:17:37 -0600

    Update frequencyvalidator, add validator spec.

--- app/models/concerns/frequency_validator.rb
+++ app/models/concerns/frequency_validator.rb
@@ -2,17 +2,17 @@
 
 class FrequencyValidator < ActiveModel::Validator
   def validate(record)
-    if (record.frequency.nil?)
-      record.errors.add(:frequency, "is nil")
-      return
+    return if record.frequency.nil?
+
+    # Allow use of VATSIM placeholder
+    return if record.frequency == 199.998
+
+    if (record.frequency.to_d < 118) || (record.frequency.to_d > 137)
+      record.errors.add :frequency, "is outside of the aviation band"
     end
 
-    if ((record.frequency.to_d < 118) || (record.frequency.to_d > 137)) && (record.frequency != 199.998)
-      record.errors.add(:frequency, "is outside of the aviation band")
-    end
-
-    unless record.frequency.to_d % 0.0125 == 0
-      record.errors.add(:frequency, "is not compliant with 12.5KHz spacing")
+    unless (record.frequency.to_d % 0.0125 == 0)
+      record.errors.add :frequency, "is not compliant with 12.5KHz spacing"
     end
   end
 end
