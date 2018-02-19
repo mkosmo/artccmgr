5264061e1196effa99830ffa7ac3edf7cdc16cea
Matthew J. Kosmoski <mkosmo@gmail.com>
2018-02-19 00:33:13 -0600

    fix rubocop offenses.

--- app/models/concerns/frequency_validator.rb
+++ app/models/concerns/frequency_validator.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 class FrequencyValidator < ActiveModel::Validator
   def validate(record)
-    if ( record.frequency.nil? )
-      record.errors.add(:frequency, 'is nil')
+    if (record.frequency.nil?)
+      record.errors.add(:frequency, "is nil")
       return
     end
 
-    if ( record.frequency.to_d < 118 or record.frequency.to_d > 137 ) and record.frequency != 199.998
-      record.errors.add(:frequency, 'is outside of the aviation band')
+    if ((record.frequency.to_d < 118) || (record.frequency.to_d > 137)) && (record.frequency != 199.998)
+      record.errors.add(:frequency, "is outside of the aviation band")
     end
 
     unless record.frequency.to_d % 0.0125 == 0
-      record.errors.add(:frequency, 'is not compliant with 12.5KHz spacing')
+      record.errors.add(:frequency, "is not compliant with 12.5KHz spacing")
     end
   end
 end
