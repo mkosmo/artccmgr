c904795ff417a514b3511bb0ff97dcdbb47bb2cd
Matthew J. Kosmoski <mkosmo@gmail.com>
2018-02-20 11:42:10 -0600

    add spec build

--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Setting, type: :model do
     expect(build(:setting)).to be_valid
   end
 
+  let(:setting) { build(:setting) }
+
   describe "ActiveRecord validations" do
     # Basic validations
     it { expect(setting).to validate_presence_of(:key) }
