abb7906cc5dc091fe9b729b7855598847a233d58
Matthew J. Kosmoski <mkosmo@gmail.com>
2018-02-19 22:47:31 -0600

    add frequency checks to position_spec.rb

--- spec/models/atc/position_spec.rb
+++ spec/models/atc/position_spec.rb
@@ -21,7 +21,6 @@ RSpec.describe Atc::Position, type: :model do
     it { expect(position).to_not allow_value("").for(:callsign) }
     it { expect(position).to_not allow_value("").for(:callsign_prefix) }
     it { expect(position).to_not allow_value("").for(:callsign_suffix) }
-    it { expect(position).to_not allow_value("").for(:frequency) }
     it { expect(position).to_not allow_value("").for(:sortorder) }
     ["DEL", "GND", "TWR", "APP", "DEP", "CTR", "FSS"].each do |suffix|
       it { expect(position).to allow_value(suffix).for(:callsign_suffix) }
@@ -32,6 +31,15 @@ RSpec.describe Atc::Position, type: :model do
     it { expect(position).to validate_length_of(:callsign_prefix).is_equal_to(3) }
     it { expect(position).to validate_length_of(:callsign_suffix).is_equal_to(3) }
     it { expect(position).to validate_length_of(:sector_id).is_at_most(3) }
+
+    # Frequency Validations
+    it { expect(position).to_not allow_value("").for(:frequency) }
+    it { expect(position).to_not allow_value(nil).for(:frequency) }
+    it { expect(position).to_not allow_value(110.0).for(:frequency) }
+    it { expect(position).to_not allow_value(119.998).for(:frequency) }
+    it { expect(position).to allow_value(199.998).for(:frequency) }
+    it { expect(position).to allow_value(121.5).for(:frequency) }
+    it { expect(position).to allow_value(123.05).for(:frequency) }
   end
 
 end
