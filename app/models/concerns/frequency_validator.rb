e55456b748b1f2277b48494395fbec07bbdeeae8
Matthew J. Kosmoski <mkosmo@gmail.com>
2018-02-19 22:29:40 -0600

    Break FrequencyValidator steps in to methods.

--- app/models/concerns/frequency_validator.rb
+++ app/models/concerns/frequency_validator.rb
@@ -7,17 +7,28 @@ class FrequencyValidator < ActiveModel::Validator
     return if record.frequency.nil?
     return if record.frequency == ""
 
-    unless record.frequency.is_a?(BigDecimal) || record.frequency.is_a?(Float)
-      record.errors.add :frequency, "is an invalid type"
-    end
+    validate_type(record)
 
     # Allow use of VATSIM placeholder
     return if record.frequency == 199.998
 
+    validate_range(record)
+    validate_steps(record)
+  end
+
+  def validate_type(record)
+    unless record.frequency.is_a?(BigDecimal) || record.frequency.is_a?(Float)
+      record.errors.add :frequency, "is an invalid type"
+    end
+  end
+
+  def validate_range(record)
     unless (118..137).include? record.frequency.to_d
       record.errors.add :frequency, "is outside of the aviation band"
     end
+  end
 
+  def validate_steps(record)
     unless record.frequency.to_d % 0.0125 == 0
       record.errors.add :frequency, "is not compliant with 12.5KHz spacing"
     end
